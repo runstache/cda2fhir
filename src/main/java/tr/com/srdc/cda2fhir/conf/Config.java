93f71aa3b7f53c494e486c3d4a771aad690c0d36
Larry Shields <shieldsl@upmc.edu>
2019-06-27 16:09:27 -0400

    Updated Config Default Class.

--- src/main/java/tr/com/srdc/cda2fhir/conf/Config.java
+++ src/main/java/tr/com/srdc/cda2fhir/conf/Config.java
@@ -21,18 +21,19 @@ package tr.com.srdc.cda2fhir.conf;
  */
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.dstu2.composite.CodingDt;
-import ca.uhn.fhir.model.dstu2.valueset.CompositionStatusEnum;
-import ca.uhn.fhir.model.dstu2.valueset.ConditionVerificationStatusEnum;
-import ca.uhn.fhir.model.dstu2.valueset.ContactPointSystemEnum;
-import ca.uhn.fhir.model.dstu2.valueset.EncounterStateEnum;
 import ca.uhn.fhir.narrative.CustomThymeleafNarrativeGenerator;
 import ca.uhn.fhir.narrative.INarrativeGenerator;
 
+import java.nio.charset.Charset;
+
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Composition.CompositionStatus;
+import org.hl7.fhir.dstu3.model.Condition.ConditionVerificationStatus;
 import org.hl7.fhir.dstu3.model.ContactPoint.ContactPointSystem;
+import org.hl7.fhir.dstu3.model.Encounter.EncounterStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.nio.charset.Charset;
+
 
 public class Config {
 
@@ -41,25 +42,30 @@ public class Config {
   // Default values for some mandatory attributes, which cannot be retrieved from
   // CDA document
   public static final String DEFAULT_COMMUNICATION_LANGUAGE_CODE_SYSTEM = "urn:ietf:bcp:47";
-  public static final ConditionVerificationStatusEnum DEFAULT_CONDITION_VERIFICATION_STATUS = ConditionVerificationStatusEnum.CONFIRMED;
-  public static final CompositionStatusEnum DEFAULT_COMPOSITION_STATUS = CompositionStatusEnum.PRELIMINARY;
+  public static final ConditionVerificationStatus DEFAULT_CONDITION_VERIFICATION_STATUS = 
+      ConditionVerificationStatus.CONFIRMED;
+  public static final CompositionStatus DEFAULT_COMPOSITION_STATUS = CompositionStatus.PRELIMINARY;
   public static final ContactPointSystem DEFAULT_CONTACT_POINT_SYSTEM = ContactPointSystem.PHONE;
-  public static final CodingDt DEFAULT_ENCOUNTER_PARTICIPANT_TYPE_CODE = new CodingDt()
-      .setSystem("http://hl7.org/fhir/v3/ParticipationType").setCode("PART").setDisplay("Participation");
-  public static final EncounterStateEnum DEFAULT_ENCOUNTER_STATUS = EncounterStateEnum.FINISHED;
-  public static final CodingDt DEFAULT_DIAGNOSTICREPORT_PERFORMER_DATA_ABSENT_REASON_CODE = new CodingDt()
-      .setSystem("http://hl7.org/fhir/data-absent-reason").setCode("unknown").setDisplay("Unknown");
+  public static final Coding DEFAULT_ENCOUNTER_PARTICIPANT_TYPE_CODE = 
+      new Coding("http://hl7.org/fhir/v3/ParticipationType", "PART", "Participation");
+  public static final EncounterStatus DEFAULT_ENCOUNTER_STATUS = EncounterStatus.FINISHED;
+  public static final Coding DEFAULT_DIAGNOSTICREPORT_PERFORMER_DATA_ABSENT_REASON_CODE = 
+      new Coding("http://hl7.org/fhir/data-absent-reason", "unknown", "Unknown");
   public static final boolean DEFAULT_IMMUNIZATION_REPORTED = false;
 
-  public static final String VALIDATION_DEFINITION_PATH = "src/main/resources/validation-min.xml.zip";
-  public static final int DEFAULT_VALIDATOR_TERMINOLOGY_SERVER_CHECK_TIMEOUT = 10000; // in milliseconds, > 0
+  public static final String VALIDATION_DEFINITION_PATH = 
+      "src/main/resources/validation-min.xml.zip";
+  // in milliseconds, > 0
+  public static final int DEFAULT_VALIDATOR_TERMINOLOGY_SERVER_CHECK_TIMEOUT = 10000; 
+  
   // if the array containing URLs doesn't give an accessible URL, this URL will be
   // used
   public static final String DEFAULT_VALIDATOR_TERMINOLOGY_SERVER_URL = "http://tx.fhir.org/r2";
   public static final String[] VALIDATOR_TERMINOLOGY_SERVER_URLS = { "http://tx.fhir.org/r2", "http://test.fhir.org/r2",
       "http://fhir.i2b2.org/srv-dstu2-0.2/api/open" };
 
-  public static final String NARRATIVE_PROPERTIES_FILE_PATH = "file:src/main/resources/narrative/customnarrative.properties";
+  public static final String NARRATIVE_PROPERTIES_FILE_PATH = 
+      "file:src/main/resources/narrative/customnarrative.properties";
 
   private static boolean generateNarrative = true;
   private static INarrativeGenerator narrativeGenerator;
@@ -82,6 +88,10 @@ public class Config {
     return fhirCtx;
   }
 
+  /**
+   * Generates a Narrative.
+   * @param generateNar Boolean
+   */
   public static void setGenerateNarrative(boolean generateNar) {
     generateNarrative = generateNar;
     if (generateNarrative) {
